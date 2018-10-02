810f8909e9b09f2bf379ae345899e7eed2101da2
jguan4 <jiajingguan@gmail.com>
2018-10-02 00:02:20 -0400

    No stacking with neural network 512 by 512

--- DEMO.py
+++ DEMO.py
@@ -12,7 +12,7 @@ for factor in factors:
 # 	for normalize in normalizes:
 # factor = factors[2]
 	normalize = normalizes[2]
-	model_name = "1001runs/Acrobot_reward_factor_{0}_straightstart/nostack/neural256".format(factor, normalize/normalized_1)
+	model_name = "1001runs/Acrobot_reward_factor_{0}_straightstart/nostack/neural512".format(factor, normalize/normalized_1)
 	model_path = DIR_PATH+"/models/"+model_name
 	if os.path.isdir(model_path):
 		continue
