214214baea3515ec1dabff64fd47b034e3a34320
fred <fred@laplace.mesa.gmu.edu>
2018-10-11 16:12:16 -0400

    Update clean up file

--- clean_up.py
+++ clean_up.py
@@ -1,21 +1,22 @@
-import os, time, sys
+import os, time, sys, re,utils
 
-path = os.path.dirname(os.path.realpath(__file__))
+path = "/home/fred/Documents/JJ_Folder/RL_dynamic/models/1001runs/Acrobot_reward_factor_1_straightstart/nostack/neural128"
 
-best_chkp_time_p = time.mktime(time.strptime("Tue Oct 2 17:38 2018","%a %b %d %H:%M %Y"))
-best_chkp_time_a = time.mktime(time.strptime("Tue Oct 2 17:39 2018","%a %b %d %H:%M %Y"))
+best_chkp_time_p = time.mktime(time.strptime("Tue Oct 2 13:54 2018","%a %b %d %H:%M %Y"))
+best_chkp_time_a = time.mktime(time.strptime("Tue Oct 2 13:55 2018","%a %b %d %H:%M %Y"))
 
 files = os.listdir(path)
-paths = [i for i in os.listdir(path) if os.path.isfile(os.path.join(path,i)) and \
-'data.chkp' in i]
+paths = [os.path.join(path,i) for i in os.listdir(path) if os.path.isfile(os.path.join(path,i)) and re.match('data.chkp', i)]
 last_modified = max(paths, key=os.path.getctime)
+last_modified_name = os.path.splitext(os.path.basename(last_modified))[0]
 
-for f in os.listdir(paths):
-	if os.stat(os.path.join(path,f)).st_mtime < best_chkp_time_a and \
-	os.stat(os.path.join(path,f)).st_mtime > best_chkp_time_p:
-		os.rename(f,"best_chkp")
-	else if os.stat(os.path.join(path,f)) == last_modified:
-		os.rename(f,"last_chkp")	
+for f in paths:
+	if os.stat(f).st_mtime < best_chkp_time_a and \
+	os.stat(f).st_mtime > best_chkp_time_p:
+		os.rename(os.path.basename(f),"best."+os.path.basename(f))
+	elif re.match(last_modified_name,f):
+		os.rename(os.path.basename(f),"last."+os.path.basename(f))	
 	else:
 		if os.path.isfile(f):
-			os.remove(os.path.join(path, f))
+			# os.remove(f)
+			print('found\n')
