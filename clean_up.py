6c6189e9bc6ad8700f45f6c84a07e4c787dd5670
fred <fred@laplace.mesa.gmu.edu>
2018-10-14 18:48:56 -0400

    update clean up file

--- clean_up.py
+++ clean_up.py
@@ -1,9 +1,9 @@
 import os, time, sys, re,utils
 
-path = "/home/fred/Documents/JJ_Folder/RL_dynamic/models/1001runs/Acrobot_reward_factor_1_straightstart/nostack/neural128"
+path = "/home/fred/Documents/JJ_Folder/RL_dynamic/models/1001runs/Acrobot_reward_factor_1_straightstart/nostack/neural512"
 
-best_chkp_time_p = time.mktime(time.strptime("Tue Oct 2 13:54 2018","%a %b %d %H:%M %Y"))
-best_chkp_time_a = time.mktime(time.strptime("Tue Oct 2 13:55 2018","%a %b %d %H:%M %Y"))
+best_chkp_time_p = time.mktime(time.strptime("Wed Oct 3 10:11 2018","%a %b %d %H:%M %Y"))
+best_chkp_time_a = time.mktime(time.strptime("Wed Oct 3 10:12 2018","%a %b %d %H:%M %Y"))
 
 files = os.listdir(path)
 paths = [os.path.join(path,i) for i in os.listdir(path) if os.path.isfile(os.path.join(path,i)) and re.match('data.chkp', i)]
@@ -14,9 +14,8 @@ for f in paths:
 	if os.stat(f).st_mtime < best_chkp_time_a and \
 	os.stat(f).st_mtime > best_chkp_time_p:
 		os.rename(f,os.path.join(path,"best."+os.path.basename(f)))
-	elif re.match(last_modified_name,os.basename(f)):
+	elif re.match(last_modified_name,os.path.basename(f)):
 		os.rename(f,os.path.join(path,"last."+os.path.basename(f)))
 	else:
 		if os.path.isfile(f):
-			# os.remove(f)
-			print('found\n')
+			os.remove(f)
