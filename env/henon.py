196bf5bc0e15e4dc2fdd35441e9d09b613223abd
jguan4 <jiajingguan@gmail.com>
2018-12-04 01:17:16 -0500

    minus reward

--- env/henon.py
+++ env/henon.py
@@ -68,14 +68,14 @@ class Henon:
 		(terminal, cat) = self._terminal()
 		info = {}
 		if cat==0:
-			reward = 0. 
+			reward = -1. 
 			info['Fixed_Point'] = None
 		elif cat == 1:
 			# reward = self.radius/0.025
-			reward = 1
+			reward = 0.
 			info['Fixed_Point'] = self.state
 		elif cat == 2:
-			reward = 0.
+			reward = -1.
 			info['Fixed_Point'] = 'Out of neighborhood'
 		info['Consecutive_Reward'] = self.consecutive_reward
 		info['Radius'] = self.radius
