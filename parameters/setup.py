526b395d3f176ebb164572ccfd341547da6b0499
jguan4 <jiajingguan@gmail.com>
2019-01-22 00:44:18 -0500

    temp

--- parameters/setup.py
+++ parameters/setup.py
@@ -64,7 +64,7 @@ setup_dict = {
 		'learning_rate': Atari_Learning_Rate,
 		'explore_rate': No_Explore,
 		'target_update_frequency': 1000,
-		'batch_size': 2048, 
+		'batch_size': 512, 
 		'memory_capacity': 1000000, 
 		'num_episodes': 10000,
 		'learning_rate_drop_frame_limit': 50000
