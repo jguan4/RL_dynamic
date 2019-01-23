9f373beacc66768f17f10445e424a2f8a675d59f
jguan4 <jiajingguan@gmail.com>
2019-01-22 21:26:10 -0500

    128 5 layers

--- parameters/architectures.py
+++ parameters/architectures.py
@@ -12,12 +12,15 @@ import numpy as np
 class Basic_Architecture:
 
     def __init__(self):
-        self.layer_sizes = [128,128]
+        self.layer_sizes = [64,64,64,64,64]
 
     def evaluate(self, input, action_size):
         neural_net_1 = tf.layers.dense(input, self.layer_sizes[0], activation=tf.nn.relu, kernel_initializer=tf.contrib.layers.xavier_initializer())
         neural_net_2 = tf.layers.dense(neural_net_1, self.layer_sizes[1], activation=tf.nn.relu, kernel_initializer=tf.contrib.layers.xavier_initializer())
-        output = tf.layers.dense(neural_net_2, action_size, activation=None, name='output')
+        neural_net_3 = tf.layers.dense(neural_net_2, self.layer_sizes[2], activation=tf.nn.relu, kernel_initializer=tf.contrib.layers.xavier_initializer())
+        neural_net_4 = tf.layers.dense(neural_net_3, self.layer_sizes[3], activation=tf.nn.relu, kernel_initializer=tf.contrib.layers.xavier_initializer())
+        neural_net_5 = tf.layers.dense(neural_net_4, self.layer_sizes[4], activation=tf.nn.relu, kernel_initializer=tf.contrib.layers.xavier_initializer())
+        output = tf.layers.dense(neural_net_5, action_size, activation=None, name='output')
         return output
 
     def __str__(self):
