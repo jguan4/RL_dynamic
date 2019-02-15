a5207d682e42d5913b42f3496705b325147d14ae
jguan4 <jiajingguan@gmail.com>
2019-02-14 21:24:46 -0500

    period 1 with flipped difference

--- env/henon.py
+++ env/henon.py
@@ -47,11 +47,11 @@ class Henon:
 		ret = False
 		
 		# self.update_radius()
-		if not np.any(self.x_bars):
-			traj_dev = np.absolute(traj[-1]-traj[-2])
-		else:
-			x_bar = np.mean(self.x_bars, axis=0)
-			traj_dev = np.absolute(traj[-1]-x_bar)
+		# if not np.any(self.x_bars):
+		traj_dev = np.absolute(traj[-1]-np.flip(traj[-2],0))
+		# else:
+		# 	x_bar = np.mean(self.x_bars, axis=0)
+		# 	traj_dev = np.absolute(traj[-1]-x_bar)
 		norm_dist = LA.norm(traj_dev,2)
 
 		# if self.period>1:
