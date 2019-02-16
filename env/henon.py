c3a55f012028fc00b75b51a022f5ce15f9ac24c7
jguan4 <jiajingguan@gmail.com>
2019-02-15 23:37:19 -0500

    so I forgot I turned the termination signal off in henon, let's rerun this

--- env/henon.py
+++ env/henon.py
@@ -45,11 +45,11 @@ class Henon:
 		info = {}
 		
 		# self.update_radius()
-		if not np.any(self.x_bars):
-			traj_dev = np.absolute(traj[-1]-np.flip(traj[-2],0))
-		else:
-			x_bar = np.mean(self.x_bars, axis=0)
-			traj_dev = np.absolute(traj[-1]-x_bar)
+		# if not np.any(self.x_bars):
+		traj_dev = np.absolute(traj[-1]-np.flip(traj[-2],0))
+		# else:
+			# x_bar = np.mean(self.x_bars, axis=0)
+			# traj_dev = np.absolute(traj[-1]-x_bar)
 		norm_dist = LA.norm(traj_dev,2)
 		reward = -norm_dist
 
@@ -67,8 +67,8 @@ class Henon:
 			# self.in_neigh = True
 			# if LA.norm(np.absolute(traj[-1]-self.x_bar))<self.terminate:
 			# if LA.norm(np.absolute(traj[-1]-x_bar))<self.terminate:
-			# if norm_dist<self.terminate:
-				# ter = True
+			if norm_dist<self.terminate:
+				ter = True
 		else:
 			info['Fixed_Point'] = None
 			# if self.in_neigh and cat == 0:
