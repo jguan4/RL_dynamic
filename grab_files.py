527d3105af3a9ce68ece75a2060eb074c1afd749
jguan4 <jiajingguan@gmail.com>
2019-03-19 20:38:56 -0400

    Update grab_files.py

--- grab_files.py
+++ grab_files.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 des_path = '/home/fred/Dropbox/Neural Network/RL/Traj/Henon/'
-sour_path = '/home/fred/Documents/JJ_Folder/RL_dynamic/models/0303runs/Henon_FE'
+sour_path = '/home/fred/Documents/JJ_Folder/RL_dynamic/models/0303runs/Henon_FE/'
 s_folders = ['Whole_P1/','Whole_P2/','Whole_P4/']
 d_folders = ['Whole_P1_FE/','Whole_P2_FE/','Whole_P4_FE/']
 items = ['frame_eps.csv','period_points.csv','training_scores.csv','traj.csv']
