f27385df4235a3130e73ef51d834790f09ed0eb9
jguan4 <jiajingguan@gmail.com>
2019-03-27 11:55:18 -0400

    Update grab_files.py

--- grab_files.py
+++ grab_files.py
@@ -3,8 +3,8 @@ import shutil
 
 des_path = '/home/fred/Dropbox/Neural Network/RL/Traj/Henon/'
 sour_path = '/home/fred/Documents/JJ_Folder/RL_dynamic/models/0318runs/Henon/'
-s_folders = ['Delay_P1_50_1024/','Delay_P1_50_2048/','Delay_P1_100_512/','Delay_P1_100_1024/','Delay_P1_100_2048/','Delay_P1_200_1024/','Delay_P1_800_1024/']
-d_folders = ['Delay_P1_50_1024/','Delay_P1_50_2048/','Delay_P1_100_512/','Delay_P1_100_1024/','Delay_P1_100_2048/','Delay_P1_200_1024/','Delay_P1_800_1024/']
+s_folders = ['Delay_P1_ChangeFreq_100to200/','Delay_P1_ChangeFreq_100to10/','Delay_P1_ChangeFreq_100to1_debug/','Delay_P1_ChangeFreq_50to200_debug/']
+d_folders = ['Delay_P1_ChangeFreq_100to200/','Delay_P1_ChangeFreq_100to10/','Delay_P1_ChangeFreq_100to1_debug/','Delay_P1_ChangeFreq_50to200_debug/']
 temp = 'ao_100u_5x64_disp5_batch1024/'
 items = ['frame_eps.csv','training_scores.csv']
 n = len(s_folders)
@@ -15,4 +15,4 @@ for ind in range(n):
 	if not os.path.exists(temp_d_f):
 		os.makedirs(temp_d_f)
 	for item in items:
-		shutil.copy( temp_s_f + temp + item, temp_d_f)
+		shutil.copy( temp_s_f + item, temp_d_f)
