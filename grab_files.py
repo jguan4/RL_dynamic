6431f6aa9819d6e4f48a48e153c71342d5c69186
jguan4 <jiajingguan@gmail.com>
2019-05-01 00:10:17 -0400

    Update grab_files.py

--- grab_files.py
+++ grab_files.py
@@ -5,8 +5,8 @@ des_path = '/home/fred/Dropbox/Neural Network/RL/Traj/Henon_Net/'
 sour_path = '/home/fred/Documents/JJ_Folder/RL_dynamic/models/0318runs/Henon_Network/'
 s_folders = ['Delay_P1_5by2048_3n_obs01_1d_squareDist/']
 d_folders = ['Delay_P1_5by2048_3n_obs01_1d_squareDist/']
-temp = 'ao_100u_5x64_disp5_batch1024/'
-items = ['period_points.csv']#,'x_bar.csv']
+temp = 'ao_100u_5x64_disp5_batch102u4/'
+items = ['temp_traj2750.csv']#,'x_bar.csv']
 n = len(s_folders)
 
 for ind in range(n):
