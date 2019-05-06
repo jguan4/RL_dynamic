821331da43df548ebe0bfbf56281259fa8ee6fe5
jguan4 <jiajingguan@gmail.com>
2019-05-06 13:02:58 -0400

    Update grab_files.py

--- grab_files.py
+++ grab_files.py
@@ -1,12 +1,12 @@
 import os
 import shutil
 
-des_path = '/home/fred/Dropbox/Neural Network/RL/Traj/Henon/'
-sour_path = '/home/fred/Documents/JJ_Folder/RL_dynamic/models/0318runs/Henon/'
-s_folders = ['Delay_P1_5by2048/']
-d_folders = ['Delay_P1_5by2048/']
+des_path = '/home/fred/Dropbox/Neural Network/RL/Traj/Henon_Net/'
+sour_path = '/home/fred/Documents/JJ_Folder/RL_dynamic/models/0318runs/Henon_Network/'
+s_folders = ['Whole_P1_5by2048_3n/']
+d_folders = ['Whole_P1_5by2048_3n/']
 temp = 'ao_100u_5x64_disp5_batch102u4/'
-items = ['x_bar.csv']#,'x_bar.csv']
+items = ['x_bar.csv','temp_traj3810.csv']#,'x_bar.csv']
 n = len(s_folders)
 
 for ind in range(n):
