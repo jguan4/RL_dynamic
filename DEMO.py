bf703d4e192601a1e44b2906c800944c3ce61966
jguan4 <jiajingguan@gmail.com>
2019-05-14 02:18:21 -0400

    0.6 discount rate

--- DEMO.py
+++ DEMO.py
@@ -17,7 +17,7 @@ DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 # direction_ind = 0
 # direct1on = [np.cos(direction_ang[direction_ind]), np.sin(direction_ang[direction_ind])]
 period = 1
-model_name = "0318runs/Henon/Whole_P{0}_5by2048_.8discount".format(period)
+model_name = "0318runs/Henon/Whole_P{0}_5by2048_.6discount".format(period)
 model_path = DIR_PATH+"/models/"+model_name
 # utils.plot_func(model_path+'/temp_traj390.csv')
 
